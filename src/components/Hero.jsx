b9a47d6e86ffef736aa586df791f6e87cf6deb36
Showrav <showravc85@gmail.com>
2026-03-11 19:43:29 +0600

    update Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>
+    <div className='flex flex-col sm:flex-row border border-gray-400'>
+        {/*Hero Left Side */}
+        <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
+        <div className='text-[#414141]'>
+            <div className='flex items-center gap'>
+
+            </div>
+        </div>
+        
+        </div>
       
     </div>
   )
