b7e2e69ba084f7cf080e192b5c09eb2d96a2657e
Showrav <showravc85@gmail.com>
2026-03-13 19:29:10 +0600

    update project

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -1,16 +1,16 @@
 import React, { useContext } from 'react';
 import { ShopContext } from '../context/ShopContext';
+import Title from './Title';
 
 const LatestCollection = () => {
   const { products } = useContext(ShopContext);
-  console.log(products);
+ 
   
   return (
-    <div>
-      {products.map((product) => (
-        <div >
-        </div>
-      ))}
+    <div className='my-10'>
+      <div className='text-center py-8 text-3xl'>
+  <Title text1={'LATEST'} text2={'COLLECTIOS'}/>
+    </div>
     </div>
   )
 }
