ef0669cd0c4046c1de23235710353921f8510611
Showrav <showravc85@gmail.com>
2026-03-13 20:51:06 +0600

    Rendering Products

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -1,23 +1,52 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { ShopContext } from '../context/ShopContext';
 import Title from './Title';
+import ProductItem from './ProductItem';
 
 const LatestCollection = () => {
-  const { products } = useContext(ShopContext);
- 
-  
-  return (
-   
-    <div className='my-10'>
-      <div className='text-center py-8 text-3xl'>
-         <p className='w-3/2 m-auto text-xs sm:text-sm md:text-base text-gray-600'> 
-  Our store offers carefully selected products that combine quality, affordability, and modern design. We believe shopping should be simple, enjoyable, and accessible to everyone. Browse our collection and find the perfect items that match your style and needs.
 
-  </p>
-   <br />
-  <Title text1={'LATEST '} text2={'COLLECTIOS'}/>
-  
-    </div>
+  const { products } = useContext(ShopContext);
+
+  const [latestProducts, setLatestProducts] = useState([]);
+
+  useEffect(() => {
+    setLatestProducts(products.slice(0, 10));
+  }, [products]);
+
+  return (
+    <div className='my-10'>
+
+      <div className='text-center py-8 text-3xl'>
+
+        <p className='w-3/2 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+          Our store offers carefully selected products that combine quality,
+          affordability, and modern design. We believe shopping should be
+          simple, enjoyable, and accessible to everyone. Browse our collection
+          and find the perfect items that match your style and needs.
+        </p>
+
+        <br />
+
+        <Title text1={'LATEST '} text2={'COLLECTIONS'} />
+
+      </div>
+
+      {/* Rendering Products */}
+
+      <div className='grid grid-cols-2 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+        {
+          latestProducts.map((item, index) => (
+            <ProductItem
+              key={index}
+              id={item._id}
+              image={item.image}
+              name={item.name}
+              price={item.price}
+            />
+          ))
+        }
+      </div>
+
     </div>
   )
 }
