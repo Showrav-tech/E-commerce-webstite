3d588c79b4d3797e9402bbc8c5b47fc39de67c74
Showrav <showravc85@gmail.com>
2026-03-14 20:24:19 +0600

    update bestseller

--- src/components/BestSeller.jsx
+++ src/components/BestSeller.jsx
@@ -1,38 +1,24 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useContext, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from './Title'
 
 const BestSeller = () => {
-    const {products}=useContext(shopContext);
-    const [bestSeller,setBestSeller]=useState([]);
+
+  const { products } = useContext(ShopContext);
+  const [bestSeller, setBestSeller] = useState([]);
+
   useEffect(()=>{
-      const bestProduct=products.filter((item)=>(item.bestseller));
-      setBestSeller(bestProduct.slice(0,5))
-  },[])
+    const bestProduct = products.filter((item)=> item.bestseller);
+    setBestSeller(bestProduct.slice(0,5))
+  },[products])
+
   return (
     <div className='my-10'>
+
       <div className='text-center py-8 text-3xl'>
-        <p className='w-3/2 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
-          Our store offers carefully selected products that combine quality,
-          affordability, and modern design. We believe shopping should be
-          simple, enjoyable, and accessible to everyone. Browse our collection
-          and find the perfect items that match your style and needs.
-        </p>
-        <br />
-        <Title text1={'LATEST '} text2={'COLLECTIONS'} />
+         <Title text1={'BEST '} text2={'SELLERS'} />
       </div>
 
-       <div className='grid grid-cols-2 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
-        {
-          latestProducts.map((item, index) => (
-            <ProductItem
-              key={index}
-              id={item._id}
-              image={item.image}
-              name={item.name}
-              price={item.price}
-            />
-          ))
-        }
-      </div>
     </div>
   )
 }
