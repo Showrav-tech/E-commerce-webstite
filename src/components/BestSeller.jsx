11c254f23af08df8f1501b20a4da0d890c578659
Showrav <showravc85@gmail.com>
2026-03-14 20:19:24 +0600

    update bestSeller

--- src/components/BestSeller.jsx
+++ src/components/BestSeller.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const BestSeller = () => {
-    
+    const {products}=useContext(shopContext);
+    const [bestSeller,setBestSeller]=useState([]);
+  useEffect(()=>{
+      const bestProduct=products.filter((item)=>(item.bestseller));
+      setBestSeller(bestProduct.slice(0,5))
+  },[])
   return (
-    <div>
-      
+    <div className='my-10'>
+      <div className='text-center py-8 text-3xl'>
+        <p className='w-3/2 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+          Our store offers carefully selected products that combine quality,
+          affordability, and modern design. We believe shopping should be
+          simple, enjoyable, and accessible to everyone. Browse our collection
+          and find the perfect items that match your style and needs.
+        </p>
+        <br />
+        <Title text1={'LATEST '} text2={'COLLECTIONS'} />
+      </div>
+
+       <div className='grid grid-cols-2 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+        {
+          latestProducts.map((item, index) => (
+            <ProductItem
+              key={index}
+              id={item._id}
+              image={item.image}
+              name={item.name}
+              price={item.price}
+            />
+          ))
+        }
+      </div>
     </div>
   )
 }
