ca86041bbb1bcbf9057858272cb9f4df8962759b
Showrav <showravc85@gmail.com>
2026-03-16 20:48:40 +0600

    Completed Letterbox

--- src/components/NewsLetterBox.jsx
+++ src/components/NewsLetterBox.jsx
@@ -3,8 +3,12 @@ import React from 'react'
 const NewsLetterBox = () => {
   return (
     <div className='text-center'>
-        <p className='text-2xl font-medium text-gray-800'>Subscribe now % get 10% off</p>
-      
+        <p className='text-2xl font-medium text-gray-800'>Subscribe now & get 10% off</p>
+      <p className='text-gray-400 mt-3'>We provide a wide collection of modern and stylish products carefully selected to ensure quality and customer satisfaction. Discover the best deals and enjoy a smooth shopping experience.</p>
+    <form className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
+       <input className='w-full sm:flex-1 outline-none' type="email" placeholder='Enter your email' required/>
+       <button type='submit'className='bg-black text-white text-xs px-10 py-4'>SUBSCRIBE</button>
+        </form>
     </div>
   )
 }
