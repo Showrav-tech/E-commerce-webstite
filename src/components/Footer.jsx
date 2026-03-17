61ed57af22eb58c6a17300be39959280bc5335d7
Showrav <showravc85@gmail.com>
2026-03-17 20:25:31 +0600

    update project

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,7 +21,12 @@ const Footer = () => {
   <li>Privacy policy</li>
 </ul>
 </div>
-
+<div>
+    <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+    <ul className='flex flex-col gap-1 text-gray-600'>
+        
+    </ul>
+</div>
       </div>
     </div>
   )
