d663fc1bd04b60c35be028d5b8934de9e1c66bc2
Showrav <showravc85@gmail.com>
2026-03-18 23:02:36 +0600

    Collection filters

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -4,7 +4,13 @@ const Collection = () => {
   const {products}=useContext(ShopContext);
   return (
     
-      <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'></div>
+      <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
+        {/**Filter options */}
+        <div className='min-w-60'>
+          <p className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS</p>
+
+        </div>
+      </div>
   
   )
 }
