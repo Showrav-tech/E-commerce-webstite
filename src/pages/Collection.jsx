658726788b5636c89548435b88540fe284a0257b
Showrav <showravc85@gmail.com>
2026-03-18 23:02:13 +0600

    Collection filter

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Collection = () => {
   const {products}=useContext(ShopContext);
   return (
-    <div>
+    
       <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'></div>
-    </div>
+  
   )
 }
 
