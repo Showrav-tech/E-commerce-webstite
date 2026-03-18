c97a5ac41f27398a7648fc2c9ca301652f9161ab
Showrav <showravc85@gmail.com>
2026-03-18 22:58:40 +0600

    Create Const Api

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const Collection = () => {
+  const {products}=useContext(ShopContext);
   return (
     <div>
-      
+      <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'></div>
     </div>
   )
 }
