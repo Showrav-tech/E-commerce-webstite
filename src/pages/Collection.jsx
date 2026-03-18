766a911d6d8a2eed53f1d53c6b0d311c9daba373
Showrav <showravc85@gmail.com>
2026-03-18 23:05:59 +0600

    update project

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -8,7 +8,8 @@ const Collection = () => {
         {/**Filter options */}
         <div className='min-w-60'>
           <p className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS</p>
-
+        {/*Category */}
+        <div className={`border border-gray-300 pl-5 py-3 mt-6`}></div>
         </div>
       </div>
   
