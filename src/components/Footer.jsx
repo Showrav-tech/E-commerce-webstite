c12b6bb40c64237a50023599e454ea8febba8600
Showrav <showravc85@gmail.com>
2026-03-18 22:55:39 +0600

    update project

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { assets } from '../assets/assets';
 
 const Footer = () => {
   return (
-    <div>
+    <div> 
       <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
         <div>
             <img src={assets.logo} className='m-5 w-32' alt="" />
