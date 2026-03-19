57d5a95b9a8c3694f065a7edc593117aa01099c1
Showrav <showravc85@gmail.com>
2026-03-19 22:16:48 +0600

    update Categories

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -20,7 +20,45 @@ const Collection = () => {
         <div
           className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden'} sm:block`} >
           <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
-          <div></div>
+          <div className='flex flex-col gap-2 text-sm font-light text-gray-700'> </div>
+        <p className='flex gap-2'>
+        <input type="checkbox" className='w-3' value={'Men'}/>
+        Men
+
+        </p>
+        
+         <p className='flex gap-2'>
+        <input type="checkbox" className='w-3' value={'Women'}/>
+        Women
+
+        </p>
+         <p className='flex gap-2'>
+        <input type="checkbox" className='w-3' value={'Kids'}/>
+        Kids
+
+        </p>
+        
+        </div>
+          {/* Type */}
+        <div
+          className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden'} sm:block`} >
+          <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
+          <div className='flex flex-col gap-2 text-sm font-light text-gray-700'> </div>
+        <p className='flex gap-2'>
+        <input type="checkbox" className='w-3' value={'Topwear'}/>
+        Topwear
+
+        </p>
+        
+         <p className='flex gap-2'>
+        <input type="checkbox" className='w-3' value={'Bottomwear'}/>
+        Bottomwear
+
+        </p>
+         <p className='flex gap-2'>
+        <input type="checkbox" className='w-3' value={'Winterwear'}/>
+        Winterwear
+        </p>
         </div>
       </div>
 
